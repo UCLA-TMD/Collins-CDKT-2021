81f1d5c8982454fd1be54e7c3d3aa1f29d224455
FilDel <>
2021-06-14 19:31:43 -0400

    separated input parameters

--- input_params.py
+++ input_params.py
@@ -1,31 +1,40 @@
 #!/usr/bin/env python
+inputparams = {}
 
-inputparams = [
- 0.85 ,  #['h1']['KPSY']['N']['uu'] =  0.85
--1.0  ,  #['h1']['KPSY']['N']['dd'] = -1.0
- 0.   ,  #['h1']['KPSY']['N']['ss'] =  0.
- 0.   ,  #['h1']['KPSY']['N']['sb'] =  0.
- 0.   , #['h1']['KPSY']['N']['db'] =  0.
- 0.   , #['h1']['KPSY']['N']['u=  0.
+inputparams['h1'] = {}
+inputparams['h1']['KPSY'] = {}
+inputparams['h1']['KPSY']['N'] = {}
+inputparams['h1']['KPSY']['N']['uu'] =  0.85
+inputparams['h1']['KPSY']['N']['dd'] = -1.0
+inputparams['h1']['KPSY']['N']['ss'] =  0.
+inputparams['h1']['KPSY']['N']['sb'] =  0.
+inputparams['h1']['KPSY']['N']['db'] =  0.
+inputparams['h1']['KPSY']['N']['ub'] =  0.
 
- 0.69 , #['h1']['KPSY']['a']['uu'] =  0.69
- 1.79 , #['h1']['KPSY']['a']['dd'] =  1.79
- 0.   , #['h1']['KPSY']['N']['ss'] =  0.
- 0.   , #['h1']['KPSY']['N']['sb'] =  0.
- 0.   , #['h1']['KPSY']['N']['db'] =  0.
- 0.   , #['h1']['KPSY']['N']['u=  0.
+inputparams['h1']['KPSY']['a'] = {}
+inputparams['h1']['KPSY']['a']['uu'] =  0.69
+inputparams['h1']['KPSY']['a']['dd'] =  1.79
+inputparams['h1']['KPSY']['a']['ss'] =  0.
+inputparams['h1']['KPSY']['a']['sb'] =  0.
+inputparams['h1']['KPSY']['a']['db'] =  0.
+inputparams['h1']['KPSY']['a']['ub'] =  0.
 
- 0.05 , #['h1']['KPSY']['b']['uu'] =  0.05
- 7.00 , #['h1']['KPSY']['b']['dd'] =  7.00
- 0.   , #['h1']['KPSY']['N']['ss'] =  0.
- 0.   , #['h1']['KPSY']['N']['sb'] =  0.
- 0.   , #['h1']['KPSY']['N']['db'] =  0.
- 0.   , #['h1']['KPSY']['N']['u=  0.
+inputparams['h1']['KPSY']['b'] = {}
+inputparams['h1']['KPSY']['b']['uu'] =  0.05
+inputparams['h1']['KPSY']['b']['dd'] =  7.00
+inputparams['h1']['KPSY']['b']['ss'] =  0.
+inputparams['h1']['KPSY']['b']['sb'] =  0.
+inputparams['h1']['KPSY']['b']['db'] =  0.
+inputparams['h1']['KPSY']['b']['ub'] =  0.
 
--0.262, #['H3']['KPSY']['N']['fav'] =  -0.262
--0.262, #['H3']['KPSY']['N']['unf'] =   -0.262
- 1.69 , #['H3']['KPSY']['a']['fav'] =   1.69
- 0.32 , #['H3']['KPSY']['a']['unf'] =   0.32
- 0.   , #['H3']['KPSY']['b']['fav'] =  0.
- 0.    #['H3']['KPSY']['b']['unf'] =  0.
-]
+inputparams['H3'] = {}
+inputparams['H3']['KPSY'] = {}
+inputparams['H3']['KPSY']['N'] = {}
+inputparams['H3']['KPSY']['N']['fav'] =  -0.262
+inputparams['H3']['KPSY']['N']['unf'] =   0.195
+inputparams['H3']['KPSY']['a'] = {}
+inputparams['H3']['KPSY']['a']['fav'] =   1.69
+inputparams['H3']['KPSY']['a']['unf'] =   0.32
+inputparams['H3']['KPSY']['b'] = {}
+inputparams['H3']['KPSY']['b']['fav'] =  0.
+inputparams['H3']['KPSY']['b']['unf'] =  0.
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-
+import numpy as np
 from iminuit import Minuit
 
 import reader as rd
 from TMDs.distrib import DISTRIB
 from input_params import inputparams
+
 
 vv=True
 
@@ -24,7 +25,30 @@ data = rd.readdata()
 def main():
     m = Minuit(
         chi_squared,
-        inputparams
+        h1Ndd  = inputparams['h1']['KPSY']['N']['dd']  ,
+        h1Nuu  = inputparams['h1']['KPSY']['N']['uu']  ,
+        h1Nss  = inputparams['h1']['KPSY']['N']['ss']  ,
+        h1Ndb  = inputparams['h1']['KPSY']['N']['db']  ,
+        h1Nub  = inputparams['h1']['KPSY']['N']['ub']  ,
+        h1Nsb  = inputparams['h1']['KPSY']['N']['sb']  ,
+        h1add  = inputparams['h1']['KPSY']['a']['dd']  ,
+        h1auu  = inputparams['h1']['KPSY']['a']['uu']  ,
+        h1ass  = inputparams['h1']['KPSY']['a']['ss']  ,
+        h1adb  = inputparams['h1']['KPSY']['a']['db']  ,
+        h1aub  = inputparams['h1']['KPSY']['a']['ub']  ,
+        h1asb  = inputparams['h1']['KPSY']['a']['sb']  ,
+        h1bdd  = inputparams['h1']['KPSY']['b']['dd']  ,
+        h1buu  = inputparams['h1']['KPSY']['b']['uu']  ,
+        h1bss  = inputparams['h1']['KPSY']['b']['ss']  ,
+        h1bdb  = inputparams['h1']['KPSY']['b']['db']  ,
+        h1bub  = inputparams['h1']['KPSY']['b']['ub']  ,
+        h1bsb  = inputparams['h1']['KPSY']['b']['sb']  ,
+        H3Nfav = inputparams['H3']['KPSY']['N']['fav'] ,
+        H3Nunf = inputparams['H3']['KPSY']['N']['unf'] ,
+        H3afav = inputparams['H3']['KPSY']['a']['fav'] ,
+        H3aunf = inputparams['H3']['KPSY']['a']['unf'] ,
+        H3bfav = inputparams['H3']['KPSY']['b']['fav'] ,
+        H3bunf = inputparams['H3']['KPSY']['b']['unf'] ,
     )
     # m.simplex()
     # m.hesse()
@@ -43,7 +67,12 @@ def main():
     print(m.errors, '\n')
 
 
-def chi_squared(params):
+def chi_squared(h1Nuu,h1Ndd,h1Nss,h1Nub,h1Ndb,h1Nsb,
+                h1auu,h1add,h1ass,h1adb,h1aub,h1asb,
+                h1buu,h1bdd,h1bss,h1bub,h1bdb,h1bsb,
+                H3Nfav,H3Nunf,H3afav,H3aunf,H3bfav,H3bunf):
+
+    params = [h1Nuu,h1Ndd,h1Nss,h1Nub,h1Ndb,h1Nsb,h1auu,h1add,h1ass,h1adb,h1aub,h1asb,h1buu,h1bdd,h1bss,h1bub,h1bdb,h1bsb,H3Nfav,H3Nunf,H3afav,H3aunf,H3bfav,H3bunf]
 
     d = DISTRIB(FFname,PDFname,params)
 
@@ -59,35 +88,52 @@ def chi_squared(params):
     data['JLAB']['error'] = np.sqrt(data['JLAB']['stat_u']**2 + data['JLAB']['syst_u']**2)
     data['JLAB']['res'] = (data['JLAB']['theo_CA'] - data['JLAB']['Collins_asym'])**2/data['JLAB']['error']**2
 
-    #-- COMPASS
-    for proj in ['x','z','p']:
+    # #-- COMPASS
+    # for proj in ['x','z','p']:
+    #
+    #     if vv: print('calulating Compass in ',proj)
+    #
+    #     theo_CA = []
+    #
+    #     for i in range(len(data['COMPASS'])):
+    #         theo_CA.append(d.Collins_Asym(data['COMPASS'][proj]['pT'][i],
+    #                                       data['COMPASS'][proj]['x'][i],
+    #                                       data['COMPASS'][proj]['z'][i],
+    #                                       data['COMPASS'][proj]['Q'][i]))
+    #
+    #     data['COMPASS'][proj]['theo_CA'] = theo_CA
+    #
+    #     data['COMPASS'][proj]['res'] = (data['COMPASS']['theo_CA'] - data['COMPASS']['Collins_asym'])**2/data['COMPASS']['error']**2
 
-        if vv: print('calulating Compass in ',proj)
+    # #-- HERMES
+    # if vv: print('calulating HERMES')
+    # theo_CA = []
+    # for i in range(len(data['HERMES'])):
+    #     theo_CA.append(d.Collins_Asym(data['HERMES']['pT'][i],
+    #                                   data['HERMES']['x'][i],
+    #                                   data['HERMES']['z'][i],
+    #                                   data['HERMES']['Q'][i]))
+    #
+    # data['HERMES']['theo_CA'] = theo_CA
+    # data['HERMES']['error'] = np.sqrt(data['HERMES']['stat_u']**2 + data['HERMES']['syst_u']**2)
+    # data['HERMES']['res'] = (data['HERMES']['theo_CA'] - data['HERMES']['Collins_asym'])**2/data['HERMES']['error']**2
 
-        theo_CA = []
-
-        for i in range(len(data['COMPASS'])):
-            theo_CA.append(d.Collins_Asym(data['COMPASS'][proj]['pT'][i],
-                                          data['COMPASS'][proj]['x'][i],
-                                          data['COMPASS'][proj]['z'][i],
-                                          data['COMPASS'][proj]['Q'][i]))
-
-        data['COMPASS'][proj]['theo_CA'] = theo_CA
-
-        data['COMPASS'][proj]['res'] = (data['COMPASS']['theo_CA'] - data['COMPASS']['Collins_asym'])**2/data['COMPASS']['error']**2
-
-    #-- HERMES
-    if vv: print('calulating HERMES')
-    theo_CA = []
-    for i in range(len(data['HERMES'])):
-        theo_CA.append(d.Collins_Asym(data['HERMES']['pT'][i],
-                                      data['HERMES']['x'][i],
-                                      data['HERMES']['z'][i],
-                                      data['HERMES']['Q'][i]))
-
-    data['HERMES']['theo_CA'] = theo_CA
-    data['HERMES']['error'] = np.sqrt(data['HERMES']['stat_u']**2 + data['HERMES']['syst_u']**2)
-    data['HERMES']['res'] = (data['HERMES']['theo_CA'] - data['HERMES']['Collins_asym'])**2/data['HERMES']['error']**2
+    #-- STAR
+    # for proj in ['j','z','p']:
+    #
+    #     if vv: print('calulating Compass in ',proj)
+    #
+    #     theo_CA = []
+    #
+    #     for i in range(len(data['COMPASS'])):
+    #         theo_CA.append(d.Collins_Asym(data['STAR'][proj]['pT'][i],
+    #                                       data['STAR'][proj]['x'][i],
+    #                                       data['STAR'][proj]['z'][i],
+    #                                       data['STAR'][proj]['Q'][i]))
+    #
+    #     data['STAR'][proj]['theo_CA'] = theo_CA
+    #
+    #     data['STAR'][proj]['res'] = (data['STAR']['theo_CA'] - data['STAR']['Collins_asym'])**2/data['STAR']['error']**2
 
 
 
@@ -96,14 +142,14 @@ def chi_squared(params):
 
     chi2 = {}
 
-    chi2['JLAB'] = sum(chi2['JLAB']['res'])
-    chi2['HERMES'] = sum(chi2['HERMSES']['res'])
-    chi2['COMPASS']={}
-    chi2['COMPASS']['x'] = chi2['COMPASS']['x']['res']
-    chi2['COMPASS']['p'] = chi2['COMPASS']['p']['res']
-    chi2['COMPASS']['z'] = chi2['COMPASS']['z']['res']
+    chi2['JLAB'] = sum(data['JLAB']['res'])
+    # chi2['HERMES'] = sum(data['HERMSES']['res'])
+    # chi2['COMPASS']={}
+    # chi2['COMPASS']['x'] = data['COMPASS']['x']['res']
+    # chi2['COMPASS']['p'] = data['COMPASS']['p']['res']
+    # chi2['COMPASS']['z'] = data['COMPASS']['z']['res']
 
-    chi2['tot'] = chi2['JLAB'] + chi2['HERMES'] + chi2['COMPASS']['x'] + chi2['COMPASS']['p'] + chi2['COMPASS']['z']
+    chi2['tot'] = chi2['JLAB'] # + chi2['HERMES'] + chi2['COMPASS']['x'] + chi2['COMPASS']['p'] + chi2['COMPASS']['z']
 
     with open('chi2.out','w') as out:
         out.write(chi2)
